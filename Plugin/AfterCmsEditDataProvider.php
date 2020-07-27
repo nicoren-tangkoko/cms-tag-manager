0624e8cad3290cbd396b3168f081b1fcbf1de80b
Grzegorz Bogusz <grzegorz.bogusz@creativestyle.pl>
2020-07-27 08:44:52 +0200

    Formatted code

--- Plugin/AfterCmsEditDataProvider.php
+++ Plugin/AfterCmsEditDataProvider.php
@@ -20,10 +20,12 @@ class AfterCmsEditDataProvider
         }
 
         foreach($result as $pageData){
-            if (isset($pageData['cms_image_teaser'])) {
-                    $imageTeaserDataArray = $this->cmsImageDataProvider->getImageData($pageData['cms_image_teaser'], \MageSuite\CmsTagManager\Model\ImageTeaser::CMS_IMAGE_TEASER_PATH);
-                    $result[$pageData['page_id']]['cms_image_teaser'] = $imageTeaserDataArray;
+            if (!isset($pageData['cms_image_teaser'])) {
+                continue;
             }
+
+            $imageTeaserDataArray = $this->cmsImageDataProvider->getImageData($pageData['cms_image_teaser'], \MageSuite\CmsTagManager\Model\ImageTeaser::CMS_IMAGE_TEASER_PATH);
+            $result[$pageData['page_id']]['cms_image_teaser'] = $imageTeaserDataArray;
         }
 
         return $result;
