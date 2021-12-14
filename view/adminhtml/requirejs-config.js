b8506cde8ec0b27d47c5a5e5904ec3cf9c3c5cd6
Paweł Ledwig <pawel.ledwig@creativestyle.pl>
2021-12-14 14:58:28 +0100

    feat: [GNP-843] CMS Page Tags input validation.

---
+++ view/adminhtml/requirejs-config.js
@@ -0,0 +1,9 @@
+var config = {
+    config: {
+        mixins: {
+            'Magento_Ui/js/lib/validation/rules': {
+                'MageSuite_CmsTagManager/js/lib/validation/rules': true
+            }
+        }
+    }
+};
